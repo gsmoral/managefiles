aa3b9945a7fec4ea40db149b52984b44646b8a85
Guillermo <gsmoral@hotmail.com>
2019-12-17 18:28:57 +0100

    Files MCm - ModelController migration

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Auth::routes();
 
 //Admin
 Route::get('/home', 'HomeController@index')->name('dashboard');
+
+//Files
+Route::get('archivos/{type}', 'FilesController@index');
+Route::get('archivos/subir', 'FilesController@showFileForm');
+Route::post('archivos/subir', 'FilesController@store');
+Route::post('archivos/editar/{id}', 'FilesController@edit');
+Route::post('archivos/aliminar/{id}', 'FilesController@destroy');
